2ee5fb165f58e689ce2de4f3430459e7e11a27f1
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-09-16 15:58:49 +0200

    Fix logging for multi and single rank configurations

--- src/icon_exclaim_perf_tools/log_import.py
+++ src/icon_exclaim_perf_tools/log_import.py
@@ -138,23 +138,34 @@ def import_nvtx_ranges(
 def import_timer_report(
     db: sqla.orm.Session,
     model_run: IconRun,
-    line_iterator: LineCursor
+    line_iterator: LineCursor,
+    single_rank: bool = False
 ) -> None:
     columns = {  # careful: order here matters
         "name": "name",
         "num_calls": "# calls",
         "time_min": "t_min",
-        # "_min_rang": "min rank",
+        "_min_rang": "min rank",
         "time_avg": "t_avg",
         "time_max": "t_max",
-        # "_max_rang": "max rank",
+        "_max_rang": "max rank",
         "_total_time_min": "total min (s)",  # numbers are equal to total time so skip
-        # "_total_time_min_rank": "total min rank",
+        "_total_time_min_rank": "total min rank",
         "time_total": "total max (s)",
-        # "_total_time_max_rank": "total max rank",
+        "_total_time_max_rank": "total max rank",
         "_total_time_avg": "total avg (s)",  # numbers are equal to total time so skip
-        # "_num_pe": "# PEs"
+        "_num_pe": "# PEs"
+    } if not single_rank else {
+        "name": "name",
+        "num_calls": "# calls",
+        "time_min": "t_min",
+        "time_avg": "t_avg",
+        "time_max": "t_max",
+        "_total_time_min": "total min (s)",  # numbers are equal to total time so skip
+        "time_total": "total max (s)",
+        "_total_time_avg": "total avg (s)",  # numbers are equal to total time so skip
     }
+
     # skip header
     header_dash_pattern = re.compile("([-]+)".join(["(\\s+)"] * (len(columns.values())+1)))
     line_iterator.skip("")
@@ -303,7 +314,7 @@ def import_model_run_log(
         elif nvtx_pattern.match(line):
             import_nvtx_ranges(db, model_run, line_iterator)
         elif line.strip().startswith("Timer report") and not line.strip().startswith("Timer report:"):
-            import_timer_report(db, model_run, line_iterator)
+            import_timer_report(db, model_run, line_iterator, single_rank=not line.strip().startswith("Timer report, ranks"))
         elif line.strip().startswith("[SUBDOMAINS]"):
             import_subdomains(db, model_run, line_iterator.revert())
 
