548c7ab4611cdd4813e542a468dd031428e4bb1b
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-12-10 11:48:31 +0100

    Restrict while loop

--- src/icon_exclaim_perf_tools/log_import.py
+++ src/icon_exclaim_perf_tools/log_import.py
@@ -177,8 +177,7 @@ def import_timer_report(
     # skip header
     header_dash_pattern = re.compile("([-]+)".join(["(\\s+)"] * (len(columns.values())+1)))
     line_iterator.skip("")
-    # Make sure we assign to the head_dash_line a line that contains dashes
-    while not re.match(r"-+", line_iterator.current_line().strip()):
+    while re.match(r"mo_.*:.*", line_iterator.current_line().strip()):
         next(line_iterator)
     header_dash_line = line_iterator.current_line()
     line_iterator.skip(header_dash_pattern, strip=False)
